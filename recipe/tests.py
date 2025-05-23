b57e5f861bb8f447c05452a631f26b5861b4ad6c
unl1ke4 <w1tcher@ukr.net>
2025-05-23 12:47:42 +0300

    Створив клас CategoryModelTest та метод test_create_category для тестування.

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Category, Recipe
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_create_category(self):
+        category = Category.objects.create(name="Dessert")
+        self.assertEqual(str(category), "Dessert")
+        self.assertEqual(list(iter(category)), list("Dessert"))
