b34f9eaaeb6a364211255fc046fa6fb44fa10902
pivassio <akeno.harada1996@gmail.com>
2025-05-23 12:38:17 +0300

    Додав модель Recipe

--- recipe/models.py
+++ recipe/models.py
@@ -7,6 +7,13 @@ class Category(models.Model):
         return self.name
 
     def __iter__(self):
-        # Припустимо, ви хочете ітеруватись по символах назви
         return iter(self.name)
 
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
