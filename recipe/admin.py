80af1e6456f88c0b9a6d8338addf0fa725abacaa
unl1ke4 <w1tcher@ukr.net>
2025-05-23 13:31:50 +0300

    Оновив admin.py

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Recipe, Category
 
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe)
+admin.site.register(Category)
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'created_at', 'updated_at')
