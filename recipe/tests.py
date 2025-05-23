c8d64a97dfb51accddca47062b8ec3df43ef216b
pivassio <akeno.harada1996@gmail.com>
2025-05-23 12:52:01 +0300

    Додав метод test_create_recipe

--- recipe/tests.py
+++ recipe/tests.py
@@ -10,3 +10,16 @@ class CategoryModelTest(TestCase):
 class RecipeModelTest(TestCase):
     def setUp(self):
         self.category = Category.objects.create(name="Main Course")
+    
+    def test_create_recipe(self):
+        recipe = Recipe.objects.create(
+            title="Borshch",
+            description="Traditional Ukrainian beet soup",
+            instructions="1. Chop vegetables\n2. Boil water\n3. Add ingredients",
+            ingredients="Beetroot, Potatoes, Carrots, Onion, Cabbage",
+            category=self.category
+        )
+        self.assertEqual(str(recipe), "Borscht")
+        self.assertEqual(recipe.category.name, "Main Course")
+        self.assertIsNotNone(recipe.created_at)
+        self.assertIsNotNone(recipe.updated_at)
